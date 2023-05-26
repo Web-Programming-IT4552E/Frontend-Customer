320f1615a7151f1a837892cf7f350b9eaf76ff01
Evmos <longdieu12x@gmail.com>
2023-05-26 22:49:53 +0700

    add sample

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Banner from '@/components/Banner';
+
+const Home = () => {
+  return (
+    <div>
+      <p>Heyy</p>
+      <Banner />
+    </div>
+  );
+};
+
+export default Home;
